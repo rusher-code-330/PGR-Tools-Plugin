d2a99f8f1ac4a69f4c16a4a80381ac8745dd503f
Rusher <rusher.musique@gmail.com>
2026-05-12 22:04:59 +0200

    Add files via upload

---
+++ ascii-text.py
@@ -0,0 +1,9 @@
+import pyfyglet
+
+REQUIRES = ["pyfiglet""]
+
+def run():
+	print("\n== ASCII TEXT ==")
+	textascii = input("| ecrit quelque chose... >")
+	result = pyfiglet.figlet_format (textascii, font = "standard")
+	print(result)
