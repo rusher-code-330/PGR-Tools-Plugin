d5d9ddab3e2a16927a716251c4880a40a115a694
Rusher <rusher.musique@gmail.com>
2026-05-12 21:47:29 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,17 +1,18 @@
 import time
-import colorama
-from colorama import init, Fore
-from colorama import init, Style
+from colorama import init, Fore, Style
 
 init()
 
 REQUIRES = ["colorama"]
 
 def run():
+
     seconds = int(input("| time ?> "))
 
     for i in range(seconds, 0, -1):
-        print(Fore.cyan +"| "+ str(i) + Style.RESET_ALL)
+
+        print(Fore.CYAN + "| " + str(i) + Style.RESET_ALL)
+
         time.sleep(1)
 
     print("| done")
